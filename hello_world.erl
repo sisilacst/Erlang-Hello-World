61fb97dbba2867cd4fae2951b526f0b3a1b4bd7f
sisila <uwusisila@gmail.com>
2015-11-27 22:48:25 +0530

    add comments

--- hello_world.erl
+++ hello_world.erl
@@ -1,5 +1,8 @@
+%module declaration same as file name
 -module(hello_world).
+% export the function then we can call it from out side the module 0 means 0 arguments
 -export([say_hello/0]).
 
+%say_hello function 
 say_hello()->
 	io:fwrite("Hello Erlang World.").
